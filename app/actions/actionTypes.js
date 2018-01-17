210bbaf14bb9ecbf8c166956c4ad3c975b8d39a5
Ruan Martinelli <martinelliruan@gmail.com>
2018-01-17 18:35:04 -0200

    remove counter example

--- app/actions/actionTypes.js
+++ app/actions/actionTypes.js
@@ -1,6 +1,3 @@
-export const DECREMENT = 'counter/DECREMENT';
-export const INCREMENT = 'counter/INCREMENT';
-
 export const FETCH_KIT_GENDERS = 'kit/FETCH_KIT_GENDERS';
 export const FETCH_KIT_SHOESIZES = 'kit/FETCH_KIT_SHOESIZES';
 export const FETCH_KIT_SHIRTSIZES = 'kit/FETCH_KIT_SHIRTSIZES';
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import counterReducer from './counterReducer';
 import kit from './kitReducer';
 
 const rootReducer = combineReducers({
-  count: counterReducer,
   kit
 });
 
